76b1798113de34aa8fc66ba5f0abcb8e62881086
Ferdinand Ismael <ferdinandismael@ferdinands-mbp.lan>
2019-08-21 12:14:17 -0400

    added some styles to header 💅

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,49 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const NavBar = styled.header`
+  display: flex;
+  justify-content: space-between;
+  padding: 0;
+  margin: 0;
+
+  h2 {
+    padding: 0;
+    margin: 0;
+  }
+`;
+
+const NavLinks = styled.ul`
+  display: grid;
+  justify-content: space-between;
+  grid-template-columns: repeat(3, 33.33%);
+  padding: 0;
+  margin: 0;
+  list-style: none;
+  width: 300px;
+  li {
+    color: black;
+    text-align: right;
+  }
+`;
 
 const Nav = () => {
   return (
-    <div>
+    <NavBar>
       <h2>Logo</h2>
-      <ul>
-        <li>Home</li>
-        <li>Favorites</li>
-        <li>Login</li>
-      </ul>
-    </div>
+      <NavLinks>
+        <Link to="/">
+          <li>Home</li>
+        </Link>
+        <Link to="/favorites">
+          <li>Favorites</li>
+        </Link>
+        <Link to="/signin">
+          <li>Login</li>
+        </Link>
+      </NavLinks>
+    </NavBar>
   );
 };
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import styled from 'styled-components';
 import Nav from './Nav';
 import Home from './Home';
 import Signin from './Signin';
 import Favorites from './Favorites';
 
+const AppWrapper = styled.div`
+  max-width: 1000px;
+  width: 90%;
+  margin: 0 auto;
+`;
+
 const App = () => {
   return (
-    <Router>
-      <Nav />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/signin" component={Signin} />
-        <Route exact path="/favorites" component={Favorites} />
-      </Switch>
-    </Router>
+    <AppWrapper>
+      <Router>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/signin" component={Signin} />
+          <Route exact path="/favorites" component={Favorites} />
+        </Switch>
+      </Router>
+    </AppWrapper>
   );
 };
 
