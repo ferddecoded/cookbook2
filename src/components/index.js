444a6b9fe6a3f22a941e3e82da34623159d0a46d
Ferdinand Ismael <ferdinandismael@ferdinands-mbp.lan>
2019-08-21 11:51:16 -0400

    added routes & react-router-dom 🔨

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Nav from './Nav';
+import Home from './Home';
+import Signin from './Signin';
+import Favorites from './Favorites';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/signin" component={Signin} />
+        <Route exact path="/favorites" component={Favorites} />
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
