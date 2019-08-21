6ea6d78ee60c0e911133737893f0fa47260f969b
Ferdinand Ismael <ferdinandismael@ferdinands-mbp.lan>
2019-08-21 11:37:57 -0400

    initial commit 🥇

---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <div>
+      <h2>Logo</h2>
+      <ul>
+        <li>Home</li>
+        <li>Favorites</li>
+        <li>Login</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Nav;
