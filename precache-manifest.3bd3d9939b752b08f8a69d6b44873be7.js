69fce7e54e1cc44c9bbcbe80fbc7c3b353c6cc5f
Ferdinand Ismael <ferdinandismael@ferdinands-mbp.lan>
2020-04-23 07:24:18 -0400

    Updates

---
+++ precache-manifest.3bd3d9939b752b08f8a69d6b44873be7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb4f40d3f3eeed8a5dd8ff21c3f36aaa",
+    "url": "/cookbook2/index.html"
+  },
+  {
+    "revision": "2916ba7efc711d8e57c1",
+    "url": "/cookbook2/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f6edafee4531efbde4ba",
+    "url": "/cookbook2/static/js/2.46281a0d.chunk.js"
+  },
+  {
+    "revision": "2916ba7efc711d8e57c1",
+    "url": "/cookbook2/static/js/main.c97bddf6.chunk.js"
+  },
+  {
+    "revision": "8b079537b3d5f0bd996a",
+    "url": "/cookbook2/static/js/runtime~main.0b1e1ad8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.856ad3d9627108d2c36974e21a5aab2f.js"
+  "/cookbook2/precache-manifest.3bd3d9939b752b08f8a69d6b44873be7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cookbook2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
