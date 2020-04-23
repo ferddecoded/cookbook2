03f7b1e3e3a1faf48af7dd03603bc35f50c23e9d
Ferdinand Ismael <ferdinandismael@ferdinands-mbp.lan>
2020-04-23 07:04:12 -0400

    Updates

---
+++ precache-manifest.403954eddfe06b5cbbe099b62868a3cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ae8931026ab5e63adaf21dc4d4ec950",
+    "url": "/cookbook2/index.html"
+  },
+  {
+    "revision": "d1fa88398233d6d5936b",
+    "url": "/cookbook2/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f6edafee4531efbde4ba",
+    "url": "/cookbook2/static/js/2.46281a0d.chunk.js"
+  },
+  {
+    "revision": "d1fa88398233d6d5936b",
+    "url": "/cookbook2/static/js/main.3b85d2cb.chunk.js"
+  },
+  {
+    "revision": "8b079537b3d5f0bd996a",
+    "url": "/cookbook2/static/js/runtime~main.0b1e1ad8.js"
+  }
+]);
